f49dc0cbca7529e08f890c0ef25e8c896474fa7a
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-03-20 19:53:00 -0300

    feat(users): import database module

--- api/src/modules/users/users.module.ts
+++ api/src/modules/users/users.module.ts
@@ -3,8 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 import { PrismaService } from 'src/shared/database/prisma.service';
+import { DatabaseModule } from 'src/shared/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, PrismaService],
 })
