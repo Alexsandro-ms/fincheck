c098543c65fbdcc59f8ae0418e0cd65e1e28f415
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-03-22 13:29:43 -0300

    refactor(users): refactor the code responsible for registering a user, moved to the authentication module

--- api/src/modules/auth/auth.service.ts
+++ api/src/modules/auth/auth.service.ts
@@ -1,21 +1,26 @@
-import { compare } from 'bcryptjs';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare, hash } from 'bcryptjs';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 import { AuthenticateDto } from './dto/authenticate.dto';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { SignupDto } from './dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private readonly useRepo: UsersRepository,
+    private readonly usersRepo: UsersRepository,
     private readonly jwtService: JwtService,
   ) {}
 
   async authenticate(authenticateDto: AuthenticateDto) {
     const { email, password } = authenticateDto;
 
-    const user = await this.useRepo.findUnique({
+    const user = await this.usersRepo.findUnique({
       where: {
         email,
       },
@@ -34,5 +39,83 @@ export class AuthService {
     const accessToken = await this.jwtService.signAsync({ sub: user.id });
 
     return { accessToken };
+  }
+  async signup(signupDto: SignupDto) {
+    const { email, name, password } = signupDto;
+
+    const emailTaken = await this.usersRepo.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email already exists');
+    }
+
+    const hashedPassword = await hash(password, 12);
+
+    const user = await this.usersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              {
+                name: 'Salário',
+                icon: 'travel',
+                type: 'INCOME',
+              },
+              {
+                name: 'Freelance',
+                icon: 'freelance',
+                type: 'INCOME',
+              },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              {
+                name: 'Alimentação',
+                icon: 'food',
+                type: 'EXPENSE',
+              },
+              {
+                name: 'Educação',
+                icon: 'education',
+                type: 'EXPENSE',
+              },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              {
+                name: 'Mercado',
+                icon: 'grocery',
+                type: 'EXPENSE',
+              },
+              {
+                name: 'Roupas',
+                icon: 'clothes',
+                type: 'EXPENSE',
+              },
+              {
+                name: 'Transporte',
+                icon: 'transport',
+                type: 'EXPENSE',
+              },
+              {
+                name: 'Viagem',
+                icon: 'travel',
+                type: 'EXPENSE',
+              },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+            ],
+          },
+        },
+      },
+    });
+
+    return {
+      name: user.name,
+      email: user.email,
+    };
   }
 }
