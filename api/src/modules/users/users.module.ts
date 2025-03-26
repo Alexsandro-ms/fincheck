33fd15822ff407690d5b92a2275f49fe9969fdad
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-03-26 12:36:25 -0300

    refactor(users): removed 'usersRepository' import

--- api/src/modules/users/users.module.ts
+++ api/src/modules/users/users.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
 import { PrismaService } from 'src/shared/database/prisma.service';
 import { DatabaseModule } from 'src/shared/database/database.module';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, PrismaService],
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
