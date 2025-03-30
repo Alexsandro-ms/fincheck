44653ef78e3827130eca46d9541e3ed6ee9f2905
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-03-30 13:09:09 -0300

    styles(frontend/authLayout): fixed card margin

--- frontend/src/view/layouts/AuthLayout.tsx
+++ frontend/src/view/layouts/AuthLayout.tsx
@@ -11,13 +11,13 @@ export function AuthLayout() {
                     <Outlet />
                 </div>
             </div>
-            <div className="w-1/2 h-full p-8 jutify-center items-center relative hidden lg:flex">
+            <div className="w-1/2 h-full p-8 mx-8 jutify-center items-center relative hidden lg:flex">
                 <img
                     src={illustration}
                     alt="ilustração do dashboard"
                     className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-4xl"
                 />
-                <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-4xl">
+                <div className="max-w-[656px] bottom-8 bg-white p-10 absolute rounded-b-4xl ">
                     <Logo className="text-[#087F5B] h-8" />
                     <p className="text-[#495057] font-medium text-xl mt-6">
                         Gerencie suas finanças pessoais de uma forma simples com
