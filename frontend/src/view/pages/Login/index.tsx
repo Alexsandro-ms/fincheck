345318fb60912d1d6d87a241f34f252d4899ab3a
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-03-30 12:08:44 -0300

    feat(frontend/page): created login page

--- frontend/src/view/pages/Login/index.tsx
+++ frontend/src/view/pages/Login/index.tsx
@@ -1,3 +1,32 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function LoginPage() {
-  return <h1>Login</h1>;
+    return (
+        <div>
+            <header className="flex flex-col items-center gap-4">
+                <h1 className="text-2xl font-bold text-[#212529] tracking-[-1px]">
+                    Entre em sua conta
+                </h1>
+                <p className="space-x-2">
+                    <span className="text-[#495057] tracking-[-0.5px]">
+                        Novo por aqui?
+                    </span>
+                    <Link
+                        to="/register"
+                        className="text-[#087F5B] tracking-[-0.5px] font-medium"
+                    >
+                        Crie uma conta
+                    </Link>
+                </p>
+            </header>
+
+            <form className="mt-[60px] flex flex-col gap-4">
+                <Input name="email" type="email" placeholder="E-mail" />
+                <Input name="password" type="password" placeholder="Password" />
+                <Button type="submit" label="Entrar" />
+            </form>
+        </div>
+    );
 }
