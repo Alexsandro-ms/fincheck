1d62af26b031dcdb2edab34c2a39fb21fd4159e5
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-08 12:51:26 -0300

    refactor(frontend/component-button): added to isLoading props to handle the disable button

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -3,9 +3,16 @@ import { cn } from "../../app/utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {
     label: string;
+    isLoading?: boolean;
 }
 
-export function Button({ className, label, ...props }: ButtonProps) {
+export function Button({
+    className,
+    isLoading,
+    disabled,
+    label,
+    ...props
+}: ButtonProps) {
     return (
         <button
             {...props}
@@ -13,6 +20,7 @@ export function Button({ className, label, ...props }: ButtonProps) {
                 "bg-[#087F5B] hover:bg-[#099268] disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium disabled:text-gray-400 disabled:cursor-not-allowed transition-all",
                 className
             )}
+            disabled={disabled || isLoading}
         >
             {label}
         </button>
