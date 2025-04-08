04264b2a8ec01892387b29812b59710dca21710c
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-08 10:28:44 -0300

    refactor(button): integrate cn utility and add support for custom className

--- frontend/src/view/components/Button.tsx
+++ frontend/src/view/components/Button.tsx
@@ -1,14 +1,18 @@
 import { ComponentProps } from "react";
+import { cn } from "../../app/utils/cn";
 
 interface ButtonProps extends ComponentProps<"button"> {
     label: string;
 }
 
-export function Button({ label, ...props }: ButtonProps) {
+export function Button({ className, label, ...props }: ButtonProps) {
     return (
         <button
             {...props}
-            className="bg-[#087F5B] hover:bg-[#099268] disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium disabled:text-gray-400 disabled:cursor-not-allowed transition-all"
+            className={cn(
+                "bg-[#087F5B] hover:bg-[#099268] disabled:bg-gray-100 px-6 h-12 rounded-2xl text-white font-medium disabled:text-gray-400 disabled:cursor-not-allowed transition-all",
+                className
+            )}
         >
             {label}
         </button>
