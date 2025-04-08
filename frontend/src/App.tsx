28a3f15a49b770a7ff384c7cf274eb67b3742743
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-08 12:41:45 -0300

    feat(frontend/app): added toaster context in application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "react-hot-toast";
 import { Router } from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -7,6 +8,7 @@ export default function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <Router />
+            <Toaster />
         </QueryClientProvider>
     );
 }
