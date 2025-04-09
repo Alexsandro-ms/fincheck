f5e505912eca79fced4fcda0a8e956d88b18b0b6
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-08 21:58:37 -0300

    docs(api/bankaccounts): created documentation for categories and users endpoints

--- api/src/modules/categories/categories.controller.ts
+++ api/src/modules/categories/categories.controller.ts
@@ -1,11 +1,35 @@
 import { Controller, Get } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { ActiveUserId } from 'src/shared/decorators/activeUserId';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Categories')
+@ApiHeader({
+  name: 'Authorization',
+  description: 'JWT token in the format: Bearer <token>',
+  required: true,
+})
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @ApiOperation({ summary: 'Search all categories' })
+  @ApiResponse({
+    status: 401,
+    description: 'Token not found',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Categories list',
+    isArray: true,
+  })
   @Get()
   findAll(@ActiveUserId() userId: string) {
     return this.categoriesService.findAllByUserId(userId);
--- api/src/modules/users/users.controller.ts
+++ api/src/modules/users/users.controller.ts
@@ -1,11 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ActiveUserId } from 'src/shared/decorators/activeUserId';
+import {
+  ApiBearerAuth,
+  ApiHeader,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Users')
+@ApiHeader({
+  name: 'Authorization',
+  description: 'JWT token in the format: Bearer <token>',
+  required: true,
+})
 @Controller('users')
 export class UsersController {
   constructor(private readonly UsersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Search user by id' })
+  @ApiResponse({
+    status: 401,
+    description: 'Token not found',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'User list by id',
+  })
   @Get('/me')
   me(@ActiveUserId() userId: string) {
     return this.UsersService.getUserById(userId);
