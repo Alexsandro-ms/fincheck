13e7b904def8632ed54d907db9e9bb3acd90f8f0
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-10 20:51:18 -0300

    refactor(frontend/auth): use useMutation for login call and improve error handling

--- frontend/src/view/pages/Login/useLoginController.ts
+++ frontend/src/view/pages/Login/useLoginController.ts
@@ -1,6 +1,10 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import toast from "react-hot-toast";
+import { authService } from "../../../app/services/authService";
+import { SigninParams } from "../../../app/services/authService/signin";
+import { useMutation } from "@tanstack/react-query";
 
 const schema = z.object({
     email: z
@@ -24,10 +28,19 @@ export function useLoginController() {
         resolver: zodResolver(schema),
     });
 
-    const handleSubmit = hookFormHandleSubmit((data) => {
-        // Aqui você pode adicionar a lógica para lidar com o envio do formulário
-        console.log("Formulário enviado com sucesso!", data);
+    const { isLoading, mutateAsync } = useMutation({
+        mutationFn: async (data: SigninParams) => {
+            return authService.signin(data);
+        },
     });
 
-    return { handleSubmit, register, errors };
+    const handleSubmit = hookFormHandleSubmit(async (data) => {
+        try {
+            await mutateAsync(data);
+        } catch {
+            toast.error("Credenciais inválidas");
+        }
+    });
+
+    return { handleSubmit, register, errors, isLoading };
 }
