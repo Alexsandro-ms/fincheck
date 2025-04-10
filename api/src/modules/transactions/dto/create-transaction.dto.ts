409aa45a22a7b34f1328bb6eb24411c0c271e76e
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-09 22:17:52 -0300

    docs(dto): add Swagger ApiProperty decorators to CreateTransactionDto

--- api/src/modules/transactions/dto/create-transaction.dto.ts
+++ api/src/modules/transactions/dto/create-transaction.dto.ts
@@ -8,31 +8,38 @@ import {
   IsUUID,
 } from 'class-validator';
 import { TransactionType } from '../entities/transaction';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTransactionDto {
+  @ApiProperty({ example: 'UUID: 10503ec5-6a74-4c81-b572-e93bedd8c1b2' })
   @IsString()
   @IsNotEmpty()
   @IsUUID()
   bankAccountId: string;
 
+  @ApiProperty({ example: 'UUID: 10503ec5-6a74-4c81-b572-e93bedd8c1b2' })
   @IsString()
   @IsNotEmpty()
   @IsUUID()
   categoryId: string;
 
+  @ApiProperty({ example: 'Energy bill' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ example: 100 })
   @IsNumber()
   @IsNotEmpty()
   @IsPositive()
   value: number;
 
+  @ApiProperty({ example: '2025-02-26T20:32:09.265Z' })
   @IsDateString()
   @IsNotEmpty()
   date: string;
 
+  @ApiProperty({ example: 'EXPENSE' })
   @IsNotEmpty()
   @IsEnum(TransactionType)
   type: TransactionType;
