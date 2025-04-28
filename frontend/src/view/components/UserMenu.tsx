fd4559bbe6408e932b8420569d3a9f1bd8e3e31c
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-04-28 20:24:05 -0300

    feat(frontend/Account-section): created user menu avatar component

---
+++ frontend/src/view/components/UserMenu.tsx
@@ -0,0 +1,9 @@
+export default function UserMenu() {
+    return (
+        <div className="bg-[#E6FCF5] rounded-full w-12 h-12 flex items-center justify-center border-[0.5px] border-[#087f5b]">
+            <span className="text-sm tracking-[-0.5px] font-medium text-[#087f5b]">
+                AL
+            </span>
+        </div>
+    );
+}
