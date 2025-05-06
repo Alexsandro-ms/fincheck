6d7a2baea9d92f65982e912987b9985f5d8e3dd4
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-05-05 21:15:38 -0300

    feat(frontend/dashboard): add DashboardContextProvider to share state across components

--- frontend/src/view/pages/Dashboard/index.tsx
+++ frontend/src/view/pages/Dashboard/index.tsx
@@ -1,23 +1,26 @@
 import { Logo } from "../../components/Logo";
 import UserMenu from "../../components/UserMenu";
 import Accounts from "./components/Accounts";
+import { DashboardContextProvider } from "./components/DashboardContext";
 import Transactions from "./components/Transactions";
 
 export function DashboardPage() {
     return (
-        <div className="w-full h-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4 ">
-            <header className="h-12 items-center justify-between flex">
-                <Logo className="h-6 text-[#087F5B]" />
-                <UserMenu />
-            </header>
-            <main className="flex-1 flex flex-col md:flex-row gap-4">
-                <div className="w-full md:w-1/2">
-                    <Accounts />
-                </div>
-                <div className="w-full md:w-1/2">
-                    <Transactions />
-                </div>
-            </main>
-        </div>
+        <DashboardContextProvider>
+            <div className="w-full h-full p-4 md:px-8 md:pb-8 md:pt-6 flex flex-col gap-4 ">
+                <header className="h-12 items-center justify-between flex">
+                    <Logo className="h-6 text-[#087F5B]" />
+                    <UserMenu />
+                </header>
+                <main className="flex-1 flex flex-col md:flex-row gap-4 max-h-full">
+                    <div className="w-full md:w-1/2">
+                        <Accounts />
+                    </div>
+                    <div className="w-full md:w-1/2">
+                        <Transactions />
+                    </div>
+                </main>
+            </div>
+        </DashboardContextProvider>
     );
 }
