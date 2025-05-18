54fd3d05cee03f932f99d50b77f7c8c9e6e3bf0a
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-05-18 20:04:51 -0300

    feat(frontend/UserMenu): create DropdownMenu component with custom animation and styling

--- frontend/src/view/components/UserMenu.tsx
+++ frontend/src/view/components/UserMenu.tsx
@@ -1,9 +1,27 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from "./DropdownMenu";
+import { useAuth } from "../../app/hooks/useAuth";
+
 export default function UserMenu() {
+    const { signout } = useAuth();
     return (
-        <div className="bg-[#E6FCF5] rounded-full w-12 h-12 flex items-center justify-center border-[0.5px] border-[#087f5b]">
-            <span className="text-sm tracking-[-0.5px] font-medium text-[#087f5b]">
-                AL
-            </span>
-        </div>
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                <div className="bg-[#E6FCF5] rounded-full w-12 h-12 flex items-center justify-center border-[0.5px] border-[#087f5b]">
+                    <span className="text-sm tracking-[-0.5px] font-medium text-[#087f5b]">
+                        AL
+                    </span>
+                </div>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content className="w-32">
+                <DropdownMenu.Item
+                    className="flex items-center justify-between"
+                    onSelect={signout}
+                >
+                    Sair
+                    <ExitIcon className="w-4 h-4" />
+                </DropdownMenu.Item>
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
     );
 }
