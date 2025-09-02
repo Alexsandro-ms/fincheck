ce0a6d28b95d62264e8c1c2234d7f025147035c7
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-09-02 20:51:45 -0300

    feat(frontend/modal): new account modal created

--- frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
+++ frontend/src/view/pages/Dashboard/modals/NewAccountModal/index.tsx
@@ -1,5 +1,8 @@
+import { ColorsDropdownInput } from "../../../../components/ColorsDropdownInput";
+import { Input } from "../../../../components/Input";
 import { InputCurrency } from "../../../../components/InputCurrency";
 import { Modal } from "../../../../components/Modal";
+import Select from "../../../../components/Select";
 import { useNewAccountModalController } from "./NewAccountModalController";
 
 export function NewAccountModal() {
@@ -8,10 +11,39 @@ export function NewAccountModal() {
     return (
         <Modal
             title="Nova Conta"
-            open={isNewAccountModalOpen ?? false}
+            open={isNewAccountModalOpen}
             onClose={closeNewAccountModal}
         >
-            <InputCurrency />
+            <form>
+                <div>
+                    <span className="text-[#868E96] tracking-[-0.5px] text-xl">
+                        Saldo
+                    </span>
+                    <div className="flex items-center gap-2">
+                        <span className="text-[#868E96] tracking-[-0.5px] text-lg">
+                            R$
+                        </span>
+                        <InputCurrency />
+                    </div>
+                </div>
+                <div className="mt-10 flex flex-col gap-4">
+                    <Input
+                        name="name"
+                        type="text"
+                        placeholder="Nome da Conta"
+                    />
+
+                    <Select
+                        placeholder="Tipo"
+                        options={[
+                            { value: "CHECKING", label: "Conta Corrente" },
+                            { value: "INVESTMENT", label: "Investimentos" },
+                            { value: "CASH", label: "Dinheiro Físico" },
+                        ]}
+                    />
+                    <ColorsDropdownInput />
+                </div>
+            </form>
         </Modal>
     );
 }
