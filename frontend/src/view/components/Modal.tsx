2193399502ca4aed9fb4c1ddd1488ad0ffb76cae
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-09-02 12:53:31 -0300

    refactor(frontend/modal): implement the pattern in the code

--- frontend/src/view/components/Modal.tsx
+++ frontend/src/view/components/Modal.tsx
@@ -1,6 +1,6 @@
-import * as Dialog from "@radix-ui/react-dialog";
-import { cn } from "../../app/utils/cn";
+import * as RdxDialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
+import { cn } from "../../app/utils/cn";
 
 interface ModalProps {
     open: boolean;
@@ -18,24 +18,24 @@ export function Modal({
     onClose,
 }: ModalProps) {
     return (
-        <Dialog.Root open={open} onOpenChange={onClose}>
-            <Dialog.Portal>
-                <Dialog.Overlay
+        <RdxDialog.Root open={open} onOpenChange={onClose}>
+            <RdxDialog.Portal>
+                <RdxDialog.Overlay
                     className={cn(
                         "fixed inset-0 bg-black/80 backdrop-blur-sm z-50"
                     )}
                 />
-                <Dialog.Title hidden />
+                <RdxDialog.Title hidden />
 
-                <Dialog.Content
+                <RdxDialog.Content
                     className={cn(
                         "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 p-6 space-y-10 bg-white rounded-2xl z-[51] outline-none shadow-[0_11px_20px_0_rgba(0,0,0,0.1),0_3px_6px_-4px_rgba(0,0,0,0.1)]",
                         "data-[state=open]:animate-contentShow w-full max-w-[400px]"
                     )}
                 >
-                    <Dialog.Description className="sr-only">
+                    <RdxDialog.Description className="sr-only">
                         {title}
-                    </Dialog.Description>
+                    </RdxDialog.Description>
                     <header className="h-12 flex items-center justify-between text-[#343A40]">
                         <button
                             className="w-12 h-12 flex items-center justify-between outline-none cursor-pointer"
@@ -51,8 +51,8 @@ export function Modal({
                         </div>
                     </header>
                     <div>{children}</div>
-                </Dialog.Content>
-            </Dialog.Portal>
-        </Dialog.Root>
+                </RdxDialog.Content>
+            </RdxDialog.Portal>
+        </RdxDialog.Root>
     );
 }
